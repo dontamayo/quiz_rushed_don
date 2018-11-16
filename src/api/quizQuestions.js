ab50f58ea4f0e5e70d01612408ddc9ad3214bf9a
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-11-16 01:46:37 -0500

    created simple questionanre

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,111 @@
+
+
+var quizQuestions = [
+  {
+      question: "What is React ?",
+      answers: [
+          {
+              type: "smart",
+              content: "front-end JavaScript library"
+              
+          },
+ 
+          {
+              type: "stellar",
+              content: "used for developing complex and interactive web and mobile UI"
+          },
+          {
+              type: "smart",
+              content: "uses the virtual DOM instead of the real DOM"
+          },
+          {
+            type: "stellar",
+            content: "All of the Above"
+        }
+      ]
+  },
+  {
+      question: "Who Created Facebook",
+      answers: [
+          {
+              type: "smart",
+              content: "Mark Zuckerberg"
+          },
+          {
+              type: "stellar",
+              content: "Steve Job"
+          },
+          {
+              type: "stellar",
+              content: "Bill Gates"
+          }
+      ]
+  },
+  {
+      question: "WHo uses React as thier Framework ? ",
+      answers: [
+          {
+              type: "Stark",
+              content: "Spotify"
+          },
+          {
+              type: "stellar",
+              content: "Netflix"
+          },
+          {
+              type: "stellar",
+              content: "AirBnB"
+
+          },
+          {
+            type: "smart",
+            content: "All of the Above"
+            
+        }
+      ]
+  },
+  {
+      question: "Choose the answer that is not related to the topic",
+      answers: [
+          {
+              type: "stellar",
+              content: "Materialize"
+          },
+          {
+              type: "brilliant",
+              content: "Bootstrap"
+          },
+          {
+              type: "stellar",
+              content: "Semantic-ui"
+          },
+          {
+            type: "smart",
+            content: "Robo-style"
+        }
+      ]
+  },
+  {
+      question: " Which Programming Language are you comfortable with ? ",
+      answers: [
+          {
+              type: "smart",
+              content: "JavaScript"
+          },
+          {
+              type: "stellar",
+              content: "Python"
+          },
+          {
+              type: "stellar",
+              content: "Ruby on Rails"
+          },
+          {
+            type: "clever",
+            content: "Swift"
+        }
+      ]
+  }
+];
+ 
+export default quizQuestions;
